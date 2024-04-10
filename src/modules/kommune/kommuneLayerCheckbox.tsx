10333ccae582672cc012767377a83aaaa23d3ed5
DavidL0509 <152031021+DavidL0509@users.noreply.github.com>
2024-04-10 19:41:58 +0200

    Kommune layers

---
+++ src/modules/kommune/kommuneLayerCheckbox.tsx
@@ -0,0 +1,41 @@
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Layer } from "ol/layer";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
+
+const kommuneLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/app01/kommuner.json",
+    format: new GeoJSON(),
+  }),
+});
+
+export function KommuneLayerCheckbox({
+  setLayers,
+}: {
+  setLayers: Dispatch<SetStateAction<Layer[]>>;
+}) {
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    if (checked) {
+      setLayers((old) => [...old, kommuneLayer]);
+    }
+    return () => {
+      setLayers((old) => old.filter((l) => l !== kommuneLayer));
+    };
+  }, [checked]);
+
+  return (
+    <div>
+      <label>
+        <input
+          type={"checkbox"}
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+        />
+        {checked ? "Hide" : "Show"} kommune layer
+      </label>
+    </div>
+  );
+}
